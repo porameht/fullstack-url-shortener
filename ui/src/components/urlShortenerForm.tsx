639c6e2f02c4ad48c0842fbd922571cd705a0b27
fr4nk <89307294+porameht@users.noreply.github.com>
2022-07-19 15:33:14 +0700

    create function handle submit

--- ui/src/components/urlShortenerForm.tsx
+++ ui/src/components/urlShortenerForm.tsx
@@ -1,5 +1,20 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { Input, Button, Box, Heading, InputGroup } from "@chakra-ui/react";
+
 function URLShortenerForm() {
-  return <div>hello</div>;
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    debugger;
+  };
+  return (
+    <Box position="relative">
+      <form onSubmit={handleSubmit}>
+        <Button type="submit">CREATE SHORT URL</Button>
+      </form>
+    </Box>
+  );
 }
 
 export default URLShortenerForm;
