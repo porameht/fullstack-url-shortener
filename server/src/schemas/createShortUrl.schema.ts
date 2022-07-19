d1d846e54397f592deaae93aebc09c646324b5ac
fr4nk <89307294+porameht@users.noreply.github.com>
2022-07-19 16:34:28 +0700

    fix bug valid url

--- server/src/schemas/createShortUrl.schema.ts
+++ server/src/schemas/createShortUrl.schema.ts
@@ -3,6 +3,7 @@ import { object, string } from "yup";
 export default object({
   body: object({
     destination: string()
+      // fix bug valid url
       .url("Must be a valid URL")
       .required("Destination is required"),
   }),
